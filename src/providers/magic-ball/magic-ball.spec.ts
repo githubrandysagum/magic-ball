c9b243437eea7c06fbda493e4e2e62e81934127f
Randy Sagum <randy.sagum@yahoo.com>
2016-10-26 04:15:08 -0700

    first commit

---
+++ src/providers/magic-ball/magic-ball.spec.ts
@@ -0,0 +1,12 @@
+import {beforeEachProviders, it, describe, expect, inject} from '@angular/core/testing';
+import {MagicBall} from './magic-ball';
+ 
+describe('Magic 8 Ball Service', () => {
+ 
+    it('should do nothing', () => {
+ 
+        expect(true).toBeTruthy();
+ 
+    });
+ 
+});
